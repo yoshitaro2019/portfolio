a6bc0e87e4d5b25afb6d66ff02a9712228fb7f0e
yoshitaro2019 <yomi_majoko@yahoo.co.jp>
2020-06-12 19:45:42 +0900

    portfolioのビュー実装
    各ポートフォリオ作品のページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
+  get 'home/bootstrap01'
+  get 'home/bootstrap02'
+  get 'home/kurisuta01'
+  get 'home/kurisuta02'
+  get 'home/chat_space'
+  get 'home/freemarket61'
   
 end
