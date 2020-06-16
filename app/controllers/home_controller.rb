7a3ef2207b3a646b4cdc61fb97af088e390c4b10
yoshitaro2019 <yomi_majoko@yahoo.co.jp>
2020-06-16 21:57:22 +0900

    about_age_add
    年齢表示をruby実装

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -22,7 +22,12 @@ class HomeController < ApplicationController
   end
 
   def about 
-
+    @user_name = "Michiko Sakai"
+    @birthday = "1986/01/07"
+    date_format = "%Y%m%d"
+    birthday = @birthday.to_date
+    @age = (Date.today.strftime(date_format).to_i - birthday.strftime(date_format).to_i) / 10000
   end
+
   
 end
